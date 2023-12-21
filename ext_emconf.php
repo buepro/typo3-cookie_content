c142d847d50c03f66598b68ac79079a537ca7f5a
Roman Büchler <rb@buechler.pro>
2023-12-21 10:48:46 +0200

    [RELEASE] v0.1.0

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the composer package buepro/typo3-cookie-content.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title'            => 'Cookie content',
+    'description'      => 'This extension is intended to make websites GDPR compliant by not rendering data critical content by default. Instead a placeholder message is shown helping the user to understand the legal context. Requires the bootstrap package.',
+    'category'         => 'fe',
+    'version'          => '0.1.0',
+    'state'            => 'beta',
+    'author'           => 'Roman Büchler',
+    'author_email'     => 'rb@buechler.pro',
+    'constraints'      => [
+        'depends'   => [
+            'typo3'                 => '11.5.0-12.99.99',
+            'bootstrap_package'     => '12.0.10-14.99.99',
+        ],
+    ],
+];
